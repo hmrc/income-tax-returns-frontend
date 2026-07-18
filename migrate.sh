0809f8a5a3cb121312980625219d2b787c311b38
Chrissystk <18244373+Chrissystk@users.noreply.github.com>
2026-07-18 13:55:05 +0100

    MIPR-2871 updated migrate script

--- migrate.sh
+++ migrate.sh
@@ -7,6 +7,9 @@ echo "Copying files to tmp folder which we don't want to overwrite"
 cp -r app/common/config/ExternalRedirectHelper.scala tmp/.
 cp -r app/common/config/FrontendAppConfig.scala tmp/.
 cp app/common/models/incomeSourceDetails/IncomeSourceDetailsResponse.scala tmp/.
+cp app/common/connectors/FeatureSwitchConnector.scala tmp/.
+cp cp app/common/models/admin/FeatureSwitchName.scala tmp/.
+cp app/common/services/admin/FeatureSwitchService.scala tmp/.
 
 
 echo "Removing main code from the app folder"
@@ -24,6 +27,9 @@ echo "Copying files back from tmp folder to app folder"
 cp tmp/ExternalRedirectHelper.scala app/common/config/.
 cp tmp/FrontendAppConfig.scala app/common/config/.
 cp tmp/IncomeSourceDetailsResponse.scala app/common/models/incomeSourceDetails/.
+cp tmp/FeatureSwitchConnector.scala app/common/connectors/.
+cp tmp/FeatureSwitchName.scala app/common/models/admin/.
+cp tmp/FeatureSwitchService.scala app/common/services/admin/.
 
 echo "Removing unused files"
 rm app/shared/models/UIJourneySessionData.scala
@@ -40,6 +46,10 @@ rm -rf tmp/*
 echo "Starting cut over of unit tests from income-tax-view-change-frontend"
 echo "Copying files to tmp folder which we don't want to overwrite"
 cp test/common/models/IncomeSourceDetailsModelSpec.scala tmp/.
+cp test/common/config/featureswitch/FeatureSwitchingSpec.scala tmp/.
+cp test/common/admin/FeatureSwitchNameSpec.scala tmp/.
+cp test/common/services/admin/FeatureSwitchServiceSpec.scala tmp/.
+cp test/common/mocks/connectors/MockFeatureSwitchConnector.scala tmp/.
 
 echo "Removing current unit tests"
 
@@ -56,6 +66,10 @@ cp -r ../income-tax-view-change-frontend/test/resources test/.
 
 echo "Copying files back from tmp folder to test folder"
 cp tmp/IncomeSourceDetailsModelSpec.scala test/common/models/.
+cp tmp/FeatureSwitchingSpec.scala test/common/config/featureswitch/.
+cp tmp/FeatureSwitchNameSpec.scala test/common/admin/.
+cp tmp/FeatureSwitchServiceSpec.scala test/common/services/admin/.
+cp tmp/MockFeatureSwitchConnector.scala test/common/mocks/connectors/.
 
 echo "Removing unused files"
 rm -rf test/shared/implicits
